b143c60d524c68c238238810b922f8e58a447fe3
Ishro Dev <dev@ishro.com>
2024-10-24 19:19:57 +0400

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ height: "100%", width: "100%", boxSizing: "border-box", padding: 0, margin: 0 }}>
-      <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" height={1000} width={1800} />
+      <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" height={1000} width={1800} alt='apehouse' />
     </div>
   );
 }
