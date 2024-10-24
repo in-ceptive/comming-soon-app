3e599e36b66534b7a83a7f8277e72234e67a9b76
Ishro Dev <dev@ishro.com>
2024-10-24 19:44:24 +0400

    fixing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw", boxSizing: "border-box", padding: 0, margin: 0 }}>
-      <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" height={1000} width={1800} alt='apehouse' />
+      <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" style={{ marginLeft: "-15vw", height: "100vh", width: "115vw", }} alt='apehouse' />
     </div>
   );
 }
