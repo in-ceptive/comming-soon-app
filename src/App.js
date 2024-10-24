faa786675a639742efd0e2629844b95998022891
Ishro Dev <dev@ishro.com>
2024-10-24 19:37:23 +0400

    dimension issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{ height: "100%", width: "100%", boxSizing: "border-box", padding: 0, margin: 0 }}>
+    <div style={{ height: "100vh", width: "100vw", boxSizing: "border-box", padding: 0, margin: 0 }}>
       <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" height={1000} width={1800} alt='apehouse' />
     </div>
   );
