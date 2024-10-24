9df3484f34463198a5e5c4959e63f3247e2e6667
Ishro Dev <dev@ishro.com>
2024-10-24 19:49:13 +0400

    sda

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw", boxSizing: "border-box", padding: 0, margin: 0 }}>
-      <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" style={{ marginLeft: "-15vw", height: "100vh", width: "115vw", }} alt='apehouse' />
+      <img src="https://storage.googleapis.com/3d-container/comingsoon-ezgif.com-video-to-gif-converter.gif" style={{ height: "100vh", width: "100vw", }} alt='apehouse' />
     </div>
   );
 }
